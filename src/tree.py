6f7d84caaa6186360822e74c96f56a077fa149f8
IDEA-PCJACINTHO\MariaClara <mcmachry@gmail.com>
2017-12-17 02:31:31 -0200

    coisas

--- src/tree.py
+++ src/tree.py
@@ -4,14 +4,30 @@ class Nodo():
         self.filhos = [None for x in producao]
 
     def gera_filho(self, variavel, producao):
-        for items in self.producao:
-            if variavel in self.producao:
-                if self.filhos[self.producao.index(variavel)] != None:
-                    self.filhos[self.producao.index(variavel)] = Nodo(producao)
+        for index, item  in enumerate(self.producao):
+            if variavel == item and self.filhos[index] != None:
+                self.filhos[index] = producao
+                break
+            
+    #Diz se o nodo atual está completo em produções possiveis na sua árvore
+    def todos_filhos(self):
+        for index, item in enumerate(self.filhos):
+            if item == None and self.producao[index].isupper():
+                return False
+
+        return True   
+   def primeiro_livre(self ):
+        if self.todos_filhos():
 
 
 class Arvore():
     def __init__(self):
         self.raiz = Nodo()
 
-    def adiciona_nodo(self, )
+    def adiciona_nodo(self, variavel, producao):
+        if self.raiz.todos_filhos():
+            
+        else:
+            self.raiz.gera_filho(variavel,producao)
+
+ 
