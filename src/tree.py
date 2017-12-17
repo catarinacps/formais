b8b379dc86a71ffc238e064c5c72738f9d825342
CaioFRodrigues <caio_f.r@hotmail.com>
2017-12-17 19:15:28 -0200

    Tree made!

--- src/tree.py
+++ src/tree.py
@@ -1,14 +1,19 @@
 class Nodo():
-    def __init__(self, producao=None):
+    def __init__(self, producao):
         self.producao = producao
         self.filhos = [None for x in producao]
-
+        self.proximo = 0                  # Variavel que determina qual o próximo da árvore a ser modificado
     def gera_filho(self, variavel, producao):
-        for index, item in enumerate(self.producao):
-            if variavel == item and self.filhos[index] != None:
-                self.filhos[index] = producao
-                break
+        if not self.todos_filhos():
+            if variavel == self.producao[self.proximo] and self.filhos[self.proximo] == None:
+                nodo_filho = Nodo(producao)
+                self.filhos[self.proximo] = nodo_filho
+                self.proximo += 1
+                while not self.todos_filhos() and self.producao[self.proximo].islower():
+                    self.proximo += 1
 
+                return True
+        return False
     # Diz se o nodo atual está completo em produções possiveis na sua árvore
     def todos_filhos(self):
         for index, item in enumerate(self.filhos):
@@ -16,17 +21,21 @@ class Nodo():
                 return False
         return True
 
-    def primeiro_livre(self):
-        if self.todos_filhos():
-            pass
-
 
 class Arvore():
-    def __init__(self):
-        self.raiz = Nodo()
+    def __init__(self, producao_base):
+        self.raiz = Nodo(producao_base)
 
-    def adiciona_nodo(self, variavel, producao):
-        if self.raiz.todos_filhos():
-            pass
+    def adiciona_nodo(self, nodo, variavel, producao):
+        if nodo.gera_filho(variavel, producao):
+            return True
         else:
-            self.raiz.gera_filho(variavel, producao)
+            for filho in nodo.filhos:
+                if filho != None and self.adiciona_nodo(filho, variavel, producao):
+                    return True
+            
+    #Dado um terminal, move o próximo para o símbolo seguinte quando o próximo está apontando para esse terminal
+    def move_proximo(self, terminal):
+
+        return False
+
